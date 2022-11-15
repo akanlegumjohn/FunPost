c33f0a0e947fad252f1565395db2e108a288bdea
Akanlegum John <akanlegumjohn2@gmail.com>
2022-11-15 21:29:02 +0000

    Added user object data model using mongoose

---
+++ backend/models/userModule.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Users', userSchema);
